b329c9c412d7f9172d20883ca4f209f1e452bf36
denmaseno <agus@senomas.com>
2022-05-29 23:44:50 +0700

    add yaml lsp

--- lua/custom/plugins/lspconfig.lua
+++ lua/custom/plugins/lspconfig.lua
@@ -4,7 +4,7 @@ M.setup_lsp = function(attach, capabilities)
    local lspconfig = require "lspconfig"
 
    -- lspservers with default config
-   local servers = { "gopls", "graphql", "rust_analyzer", "dartls" }
+   local servers = { "gopls", "graphql", "rust_analyzer", "dartls", "yamlls" }
 
    for _, lsp in ipairs(servers) do
       lspconfig[lsp].setup {
