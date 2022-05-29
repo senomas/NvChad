42aa9b466cc28e1d98193f84ce6c33459071565d
denmaseno <agus@senomas.com>
2022-05-29 23:59:04 +0700

    add more language servers

--- lua/custom/plugins/lspconfig.lua
+++ lua/custom/plugins/lspconfig.lua
@@ -4,7 +4,7 @@ M.setup_lsp = function(attach, capabilities)
    local lspconfig = require "lspconfig"
 
    -- lspservers with default config
-   local servers = { "gopls", "graphql", "rust_analyzer", "dartls", "yamlls" }
+   local servers = { "gopls", "graphql", "rust_analyzer", "dartls", "yamlls", "jsonls", "html", "java_language_server", "kotlin_language_server" }
 
    for _, lsp in ipairs(servers) do
       lspconfig[lsp].setup {
